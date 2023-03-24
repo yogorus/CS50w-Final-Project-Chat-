02a07b4cbc9f7c6b974121d9ad852c96d17081af
yogorus <yogorus@gmail.com>
2023-03-24 19:48:21 +0300

    add bootstrap

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,10 +1,29 @@
+import { Nav, Navbar, NavLink} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 
 const Layout = ({children}) => {
     return (
         <>
+            <NavigationBar />
             <main>{children}</main>
         </>
+    )
+}
+
+function NavigationBar() {
+    return (
+        <Navbar collapseOnSelect expand="sm" bg='dark' variant='dark' className='p-2'>
+            <Navbar.Brand href="#home">JustChat</Navbar.Brand>
+            <Navbar.Toggle aria-controls='navbarScroll' data-bs-target='#navbarScroll'/>
+            <Navbar.Collapse id='navbarScroll'>
+                <Nav>
+                    <NavLink as={Link} to="/">Home</NavLink>
+                    <NavLink as={Link} to="/room/create">Create Room</NavLink>
+                    <NavLink as={Link} to="/login">Log In</NavLink>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 
